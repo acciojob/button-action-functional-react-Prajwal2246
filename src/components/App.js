d9440a52e57e863fa7ad32cdbf33bc0238ac1685
Prajwal2246 <janbandhuprajwal28@gmail.com>
2026-02-01 19:47:47 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,23 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [show, setShow] = useState(false);
+  const para =
+    "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy";
+
+  function handleClick() {
+    setShow((prev) => !prev);
+  }
   return (
     <div id="main">
-      // Do not alter the main div
+      {/* // Do not alter the main div */}
+      <button id="click" onClick={handleClick}>
+        Click me
+      </button>
+      {show && para}
     </div>
   );
 }
-
 
 export default App;
